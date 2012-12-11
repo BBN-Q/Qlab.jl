afd0e47e85e8b41af509a5406be0ecc0b6a756c3
Blake R. Johnson <bjohnson@bbn.com>
2012-12-11 17:20:36 -0500

    First commit. Adding Macros module.

---
+++ Macros.jl
@@ -0,0 +1,22 @@
+module Macros
+using Base
+
+export @timeit
+
+macro timeit(ex)
+    quote
+        # run once to make sure it passes through JIT
+        $(esc(ex))
+        singleRunTime = @elapsed $(esc(ex))
+        # repeat enough times such that it takes about a second, but at least 3 times
+        numRepeats = convert(Int32, max(3, floor(1.0/singleRunTime)))
+        times = zeros(numRepeats)
+        for ct = 1:numRepeats
+            times[ct] = @elapsed $(esc(ex))
+        end
+        println("Mean: $(mean(times)), Best: $(min(times))")
+        mean(times)
+    end
+end
+
+end
---
+++ README.md
@@ -0,0 +1,4 @@
+Julia-Tools
+==========
+
+Generically usual tools and macros for Julia code.
