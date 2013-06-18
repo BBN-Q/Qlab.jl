2fde11426dd1e0968d52c940c8de52f3617a37c4
Blake R. Johnson <bjohnson@bbn.com>
2013-06-18 14:48:11 -0400

    Parse data file headers and keep raw data in returned structure.

--- load_data.jl
+++ load_data.jl
@@ -1,4 +1,4 @@
-using HDF5
+using HDF5, JSON
 
 function load_data(filename)
 
@@ -7,13 +7,13 @@ function load_data(filename)
 		version = read(attrs(f)["version"])[1]
 		@assert version == 2
 
-		header = read(f["header"])
+		header = parse(read(f["header"])[1])
 		nbrDataSets = read(attrs(f)["nbrDataSets"])[1]
 		data = cell(nbrDataSets)
 
 		for ct in 1:nbrDataSets
 			raw_data = read(f["DataSet$(ct)/real"]) + im*read(f["DataSet$(ct)/real"])
-			data[ct] = {"abs_Data" => abs(raw_data), "phase_Data" => 180/pi * angle(raw_data)}
+			data[ct] = {"data" => raw_data}
 			data[ct]["xpoints"] = read(f["DataSet$(ct)/xpoints"])
 			data[ct]["xlabel"] = read(attrs(f["DataSet$(ct)/xpoints"])["label"])
 			if ndims(raw_data) > 1
@@ -31,7 +31,7 @@ function load_data(filename)
 			data = data[1]
 		end
 
-		data
+		header, data
 	end
 
 end
