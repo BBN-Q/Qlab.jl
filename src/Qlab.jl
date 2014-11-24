27550abfe68e001cba20714ecb5efcab0c5689bb
Marcus P S <marcusps@users.noreply.github.com>
2014-11-24 16:00:36 -0500

    Include and export read_records

--- src/Qlab.jl
+++ src/Qlab.jl
@@ -1,11 +1,11 @@
 module Qlab
 
-	export load_data, KT_estimation, pauliAlphabet, filter_records, comp_to_paulis, savitsky_golay
+	export load_data, KT_estimation, pauliAlphabet, filter_records, comp_to_paulis, savitsky_golay, read_records
 
 	include("load_data.jl")
 	include("KT_estimation.jl")
 	include("pauliAlphabet.jl")
 	include("comp_to_paulis.jl")
 	include("SavitskyGolay.jl")
-	
+	include("read_records.jl")
 end
