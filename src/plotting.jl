6680bc5471eb9dec02f3bca003e5951904d0df51
dieris <driste@bbn.com>
2016-08-10 10:39:13 -0400

    Common plots
    Shortcuts for commonly used plots

---
+++ src/plotting.jl
@@ -0,0 +1,54 @@
+#collection of commonly used plots
+function plot_ss_hists(shots_0, shots_1)
+  #single-shot readout histograms
+  hist_0 = kde(shots_0[:])
+  hist_1 = kde(shots_1[:])
+  fidelity = get_fidelity(shots_0, shots_1)
+  sns.set(style="ticks")
+  sns.set_style(Dict("xtick.direction" => "in"))
+  sns.set_style(Dict("ytick.direction" => "in"))
+  w=4
+  figure(figsize=(w,w/1.4))
+  plot(hist_0,label=L"$|0\rangle$")
+  plot(hist_1,label=L"$|1\rangle$")
+  ylabel("Counts")
+  xlabel("Homodyne voltage (a.u.)")
+  lgd = legend()
+  annotate(@sprintf("Fid. = %0.2f", fidelity),
+  xy=[0.1;0.7],
+  xycoords="axes fraction",
+  xytext=[0,10],
+  textcoords="offset points",
+  fontsize=10.0,
+  ha="left",
+  va="center")
+  return hist_0, hist_1
+end
+
+
+function plot2D(data, quad = 'real'; normalize=false)
+  fig = figure("pyplot_surfaceplot",figsize=(5,3))
+  ax = gca()
+  ax[:ticklabel_format](useOffset=false)
+  if quad == 'real'
+    data = real(data["data"])
+  elseif quad == 'imag'
+    data = imag(data["data"])
+  elseif quad == 'amp'
+    data = abs(data["data"])
+  end
+  if normalize
+    data/=data[:,1]
+  end
+  xpoints = repmat(data["xpoints"],1,length(data["ypoints"]))
+  ypoints = repmat(data["ypoints"]',length(data["xpoints"]),1)
+  pcolormesh(xpoints, ypoints, data',cmap = "terrain")
+  colorbar()
+  xlabel(data["xlabel"])
+  ylabel(data["ylabel"])
+  xlim([minimum(xpoints),maximum(xpoints)])
+  ylim([minimum(ypoints),maximum(ypoints)])
+  @printf("Max fid. = %.3f\n", maximum(data_re))
+  @printf("Readout freq. = 6.3632 GHz")
+  title("Cavity frequency vs readout power")
+end
