fd9d51615ba280f1134cd97a31867d032b28a776
Marcus P S <msilva@bbn.com>
2016-11-16 16:36:45 -0500

    Made test a little more readable

--- test/test-digitize.jl
+++ test/test-digitize.jl
@@ -24,5 +24,17 @@ function test_digitize(sep; C=1000, K=1000)
 
 end
 
-@test all(map(x->isapprox(x,0.35,atol=5e-2),mean(reduce(hcat,[[test_digitize(0.75)...] for _ in 1:1000]),2)))
-@test all(map(x->isapprox(x,0.40,atol=5e-2),mean(reduce(hcat,[[test_digitize(0.50)...] for _ in 1:1000]),2)))
+# run a bunch of tests, store each as a column of a matrix, take the mean
+# the rows are: 
+#   - predicted error rate (asymptotic)
+#   - observed error rate for :max mode
+#   - observed error rate for :equal mode
+#   - estimated error rate based on cals for :max mode (0 preps)
+#   - estimated error rate based on cals for :max mode (1 preps)
+#   - estimated error rate based on cals for :equal mode (0 preps)
+#   - estimated error rate based on cals for :equal mode (1 preps)
+test1 = mean(reduce(hcat,[[test_digitize(0.75)...] for _ in 1:1000]),2)
+test2 = mean(reduce(hcat,[[test_digitize(0.50)...] for _ in 1:1000]),2)
+# make sure these are all close to the prediction
+@test all(map(x->isapprox(x,0.35,atol=5e-2),test1))
+@test all(map(x->isapprox(x,0.40,atol=5e-2),test2))
