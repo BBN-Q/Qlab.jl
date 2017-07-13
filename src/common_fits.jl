7222fc8a1b286eccbf153b805cebefa6368ebd94
gribeill <guilhem.ribeill@gmail.com>
2017-07-13 17:43:19 -0400

    Refactor fits.

--- src/common_fits.jl
+++ src/common_fits.jl
@@ -1,4 +1,15 @@
+# Copyright 2017 Raytheon BBN Technologies
+#
+# Licensed under the Apache License, Version 2.0 (the "License");
+# you may not use this file except in compliance with the License.
+# You may obtain a copy of the license at
+#
+#		http://www.apache.org/licenses/LICENSE-2.0
+
 using LsqFit
+
+## TODO: Devise a way to get specify a fit as a string...
+## TODO: Fix types...
 
 """
 	FitResult type that stores the output of a fit.
@@ -13,19 +24,40 @@ immutable FitResult
 end
 
 """
-	Helper function to return a fit to a model.
-"""
-function generic_fit(xpts, ypts, model, initial_guess, fit_params::Dict{String,Float64}, model_string::String; yvars=[])
+   `generic_fit(xpts, ypts, model, initial_guess, fit_params, model_string; yvars=[])`
 
+Helper function to return a fit to a model.
+
+# Arguments
+* xpts: Independent fit variable.
+* ypts: Dependent fit variable.
+* model: Fit function(x, p) of data x and parameter vector p.
+* initial_guess: Initial guess of fit parameters.
+* fit_params::function(Array{Float64} => Dict{String, Float64}): Result dict of fit parameters.
+* model_string::String: String indicating fit function.
+* yvars: Variance of y points. Defaults to empty vector.
+
+# Returns
+* A FitResult object for the specified fit.
+"""
+function generic_fit(xpts, ypts, model, initial_guess, fit_params, model_string::String; yvars=[])
 	@assert length(xpts) == length(ypts) "X and Y data length must match."
 	if isempty(yvars)
-			result = curve_fit(model, xpts, ypts, p_guess)
+			result = curve_fit(model, xpts, ypts, initial_guess)
+			errors = estimate_errors(result)
+			sq_error = sum(((model(xpts, result.param) - ypts).^2))
 	else
 			@assert length(ypts) == length(yvars) "Y data and Y variance lengths must match."
 			result = curve_fit(model, xpts, ypts, 1./sqrt(yvars), initial_guess)
+			errors = estimate_errors(result)
+			sq_error = sum(((model(xpts, result.param) - ypts).^2)./yvars)
 	end
-	errors = estimate_errors(result)
-	sq_error = sum(((model(xpts, result.param) - ypts).^2)./yvars)
+	# Compute badness of fit:
+	# Under the null hypothesis (that the model is valid and that the observations
+	# do indeed have Gaussian statistics), the mean squared error is χ² distributed
+	# with `dof` degrees of freedom. We can quantify badness-of-fit in terms of how
+	# far the observed MSE is from the expected value, in units of σ = 2dof (the expected
+	# standard deviation for the χ² distribution)
 	dof = length(xpts)-length(initial_guess)
 	Nσ = sq_error/sqrt(2dof) - dof/sqrt(2dof)
 	return FitResult( fit_params(result.param),
@@ -33,33 +65,28 @@ function generic_fit(xpts, ypts, model, initial_guess, fit_params::Dict{String,F
 										Nσ,
 										fit_params(errors),
 										xpts->model(xpts,result.param),
-										fit_string)
+										model_string)
 end
 
+""" `fit_t1(xpts, ypts, yvars=[])`
+Fit to exponential T₁ decay y = a exp(-t/T₁) + b.
+"""
 function fit_t1(xpts, ypts, yvars=[])
-
 	T1_fit_dict(p) = Dict("a" => p[1], "T₁" => p[2], "b" => p[3])
-
-	model(t, p) = p[1]*exp(-t ./ p[2]) + p[3]
+	model(t, p) = p[1]*exp.(-t ./ p[2]) + p[3]
 	p_guess = [ypts[1]-ypts[end], xpts[end]/3., ypts[end]]
-	result = curve_fit(model, xpts, ypts, p_guess)
-	errors = estimate_errors(result)
-
-	sq_errors = sum()
-
-	xfine = linspace(xpts[1],xpts[end],1001)
-  fit_curve = (xfine, model(xfine, result.param))
-  return result.param[2:3], errors[2:3], fit_curve
+	return generic_fit(xpts, ypts, model, p_guess, T1_fit_dict,
+										"a * exp(-t/T₁) + b", yvars=yvars)
 end
 
+""" `fit_line(xpts, ypts, yvars=[])`
+Fit to linear model y = ax + b.
+"""
 function fit_line(xpts, ypts)
 	model(t, p) = p[1]*t + p[2]
 	p_guess = [-ypts[indmin(abs(xpts))]/xpts[indmin(abs(ypts))], ypts[indmin(abs(xpts))]]
-	result = curve_fit(model, xpts, ypts, p_guess)
-	errors = estimate_errors(result)
-	xfine = linspace(xpts[1],xpts[end],1001)
-  fit_curve = (xfine, model(xfine, result.param))
-  return result.param, errors, fit_curve
+	return generic_fit(xpts, ypts, model, p_guess,
+										x->Dict("a" => x[1], "b"=>x[2]), "ax + b", yvars=yvars)
 end
 
 
@@ -71,8 +98,7 @@ fit_dict2(p) = Dict("a₁"=>p[1], "T₁"=>p[2], "f₁"=>p[3], "ϕ₁"=>p[4],
                     "a₂"=>p[5], "T₂"=>p[6], "f₂"=>p[7], "ϕ₂"=>p[8],
                     "b"=>p[9])
 
-"""
-        fit_ramsey(xpts, ypts, yvars=[])
+""" `fit_ramsey(xpts, ypts, yvars=[])`
 
 Fit data to a Ramsey decay of the form
 
@@ -81,37 +107,19 @@ Fit data to a Ramsey decay of the form
 with optional weights for each point.
 """
 function fit_ramsey(xpts, ypts, yvars=[])
-    model(t, p) = p[1]*exp(-t ./ p[2]).*cos(2π*p[3] .* t + p[4]) + p[5]
 
+		fit_dict1(p) = Dict("a"=>p[1], "T"=>p[2], "f"=>p[3], "ϕ"=>p[4], "b"=>p[5])
+    model(t, p) = p[1]*exp.(-t ./ p[2]).*cos.(2π*p[3] .* t + p[4]) + p[5]
     # Use KT estimation to get a guess for the fit
     freqs,Ts,amps = KT_estimation(ypts-mean(ypts), xpts[2]-xpts[1], 1)
-
     p_guess = [abs(amps[1]), Ts[1], freqs[1], angle(amps[1]), mean(ypts)];
 
-    if isempty(yvars)
-        result = curve_fit(model, xpts, ypts, p_guess)
-    else
-        result = curve_fit(model, xpts, ypts, 1./sqrt(yvars), p_guess)
-    end
-    errors = estimate_errors(result)
-    sq_error = sum(((model(xpts, result.param) - ypts).^2)./yvars)
+		return generic_fit(xpts, ypts, model, p_guess,
+											fit_dict1, "a*exp(-t/T).*cos(2πf .* t + ϕ) + b)", yvars=yvars)
 
-    dof = length(xpts)-5
-    Nσ = sq_error/sqrt(2dof) - dof/sqrt(2dof)
-
-    xfine = linspace(xpts[1],xpts[end],1001)
-    fit_curve = (xfine, model(xfine, result.param))
-
-    return FitResult( fit_dict1(result.param),
-                      sq_error,
-                      Nσ,
-                      fit_dict1(errors),
-                      x->model(x,result.param),
-                      "a*exp(-t ./ T).*cos(2πf .* t + ϕ) + b")
 end
 
-"""
-        fit_twofreq_ramsey(xpts, ypts, yvars=[])
+""" `fit_twofreq_ramsey(xpts, ypts, yvars=[])`
 
 Fit the Ramsey data to a one-frequency model
 
@@ -126,48 +134,23 @@ used based on Akaike's Information Criterion (AIC). This assumes
 Gaussian statistics for each observation.
 """
 function fit_twofreq_ramsey(xpts, ypts, yvars=[])
-    model2(t, p) = ( p[1] * exp(-t ./ p[2]) .* cos(2π * p[3] .*t + p[4]) +
-                    p[5] * exp(-t ./ p[6]) .* cos(2π * p[7] .*t + p[8]) + p[9] )
+		fit_dict2(p) = Dict("a₁"=>p[1], "T₁"=>p[2], "f₁"=>p[3], "ϕ₁"=>p[4],
+	                    "a₂"=>p[5], "T₂"=>p[6], "f₂"=>p[7], "ϕ₂"=>p[8],
+	                    "b"=>p[9])
+    model2(t, p) = ( p[1] * exp.(-t ./ p[2]) .* cos.(2π * p[3] .*t + p[4]) +
+                    p[5] * exp.(-t ./ p[6]) .* cos.(2π * p[7] .*t + p[8]) + p[9] )
+
     #Use KT estimation to get a guess for the fit
     freqs,Ts,amps = KT_estimation(ypts-mean(ypts), xpts[2]-xpts[1], 2)
-
     phases = angle(amps)
     amps = abs(amps)
     p_guess = [amps[1], Ts[1], freqs[1], phases[1], amps[2], Ts[2], freqs[2], phases[2], mean(ypts)]
 
-    if isempty(yvars)
-        result2 = curve_fit(model2, xpts, ypts, p_guess)
-        sq_err2 = sum((model2(xpts, result2.param) - ypts).^2)
-    else
-        result2 = curve_fit(model2, xpts, ypts, 1./sqrt(yvars), p_guess)
-        sq_err2 = sum(((model2(xpts, result2.param) - ypts).^2)./yvars)
-    end
-    errors2 = estimate_errors(result2)
-    # Compute badness of fit:
-    # Under the null hypothesis (that the model is valid and that the observations
-    # do indeed have Gaussian statistics), the mean squared error is χ² distributed
-    # with `dof2` degrees of freedom. We can quantify badness-of-fit in terms of how
-    # far the observed MSE is from the expected value, in units of σ = 2dof (the expected
-    # standard deviation for the χ² distribution)
-    dof2 = length(xpts)-9
-    Nσ2 = (sq_err2 - dof2)/sqrt(2dof2)
+    fitresult2 = generic_fit(xpts, ypts, model2, p_guess, fit_dict2,
+									"a₁*exp(-t ./ T₁).*cos(2πf₁ .* t + ϕ₁) + a₂*exp(-t ./ T₂).*cos(2πf₂ .* t + ϕ₂) + b",
+									yvars=yvars)
 
-    # Now do 1 freq fit for comparisson TODO: just call fit Ramsey
-    model1(t,p) = p[1]*exp(-t ./ p[2]).*cos(2π*p[3] .* t + p[4]) + p[5]
-    freqs,Ts,amps = KT_estimation(ypts-mean(ypts), xpts[2]-xpts[1], 1)
-
-    p_guess = [abs(amps[1]), Ts[1], freqs[1], angle(amps[1]), mean(ypts)]
-    if isempty(yvars)
-        result1 = curve_fit(model1, xpts, ypts, p_guess)
-        sq_err1 = sum((model1(xpts, result1.param) - ypts).^2)
-    else
-        result1 = curve_fit(model1, xpts, ypts, 1./sqrt(yvars), p_guess)
-        sq_err1 = sum(((model1(xpts, result1.param) - ypts).^2)./yvars)
-    end
-    errors1 = estimate_errors(result1)
-    # Compute badness of fit for this model
-    dof1 = length(xpts)-5
-    Nσ1 = (sq_err1 - dof1)/sqrt(2dof1)
+    fitresult1 = fit_ramsey(xpts, ypts, yvars)
 
     # AIC computation
     k1 = 5
@@ -175,49 +158,38 @@ function fit_twofreq_ramsey(xpts, ypts, yvars=[])
     corr(k,n) = (k+1)*(k+1)/(n-k-2)
     aicc(e,k,n) = 2 * k + e + corr(k,n)
 
-    aic = aicc(sq_err2,k2,length(xpts)) - aicc(sq_err1,k1,length(xpts))
+    aic = aicc(fitresult1.sq_error,k2,length(xpts)) - aicc(fitresult1.sq_error,k1,length(xpts))
 
-    return [ FitResult( fit_dict1(result1.param),
-                        sq_err1,
-                        Nσ1,
-                        fit_dict1(errors1),
-                        x->model1(x,result1.param),
-                        "a*exp(-t ./ T).*cos(2πf .* t + ϕ) + b"
-                        ),
-             FitResult( fit_dict2(result2.param),
-                        sq_err2,
-                        Nσ2,
-                        fit_dict2(errors2),
-                        x->model2(x,result2.param),
-                        "a₁*exp(-t ./ T₁).*cos(2πf₁ .* t + ϕ₁) + a₂*exp(-t ./ T₂).*cos(2πf₂ .* t + ϕ₂) + b"
-                        )],
+    return [ fitresult1, fitresult2,
            (aic > 0) ? 1 : 2,
-           aic
+           aic ]
 end
 
-function fit_sin(xpts, ypts)
-	model(t, p) = p[1]*sin(2π*p[2] .* t) + p[3]
-
+"""`fit_sin(xpts, ypts, yvars)`
+Fit to a sine a*sin(2πf t) + b
+"""
+function fit_sin(xpts, ypts, yvars=[])
+	model(t, p) = p[1]*sin.(2π*p[2] .* t) + p[3]
 	# Use KT estimation to get a guess for the fit
 	freqs,Ts,amps = KT_estimation(ypts, xpts[2]-xpts[1], 2)
 	idx = indmax(abs(amps))
-
 	p_guess = [amps[idx], Ts[idx], freqs[idx], mean(ypts)];
-
-	result = curve_fit(model, xpts, ypts, p_guess)
-	errors = estimate_errors(result)
-	return result.param, errors
+	return generic_fit(xpts, ypts, model, p_guess,
+										x->Dict("a"=>x[1], "f"=>x[2], "b"=>x[3]),
+										"a*sin(2πf t) + b", yvars=yvars)
 end
 
-#Fit function in McClure et al., Phys. Rev. App. 2016 (see below for details)
+## TODO: Change this to use generic_fit
+"""`fit_photon_ramsey(xpts, ypts, params)`
+Fit function in McClure et al., Phys. Rev. App. 2016. Params are:
+	1. cavity decay rate κ (MHz)
+	2. detuning Δ (MHz)
+	3. dispersive shift 2χ (MHz)
+	4. Ramsey decay time T₂* (μs)
+	5. exp(-t_meas/T₁) (μs)
+	6. initial qubit state (0/1)
+"""
 function fit_photon_ramsey(xpts, ypts, params)
-	# input params:
-	# 1 - cavity decay rate kappa (MHz)
-	# 2 - detuning Delta (MHz)
-	# 3 - dispersive shift 2Chi (MHz)
-	# 4 - Ramsey decay time T2* (us)
-	# 5 - exp(-t_meas/T1) (us), only if starting from |1>! (to include relaxation during the 1st msm't)
-	# 6 - initial qubit state (0/1)
 	params[1:3]*=2*pi #convert to angular frequencies
 	model_0(t, p) = (-imag(exp(-(1/params[4]+params[2]*1im).*t + (p[1]-p[2]*params[3]*(1-exp(-((params[1] + params[3]*1im).*t)))/(params[1]+params[3]*1im))*1im)))
 	function model(t, p)
