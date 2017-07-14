b2cf3135772147a1aa9835f2224e321ee8059ca6
gribeill <guilhem.ribeill@gmail.com>
2017-07-14 17:09:46 -0400

    Remove unnecessary subscripts, fix indentation.

--- src/common_fits.jl
+++ src/common_fits.jl
@@ -15,12 +15,12 @@ using LsqFit
 	FitResult type that stores the output of a fit.
 """
 immutable FitResult
-    fit_params::Dict{String,Float64}
-    sq_error::Float64
-    Nσ::Float64
-    errors::Dict{String,Float64}
-    fit_curve::Function
-    model_str::String
+  fit_params::Dict{String,Float64}
+  sq_error::Float64
+  Nσ::Float64
+  errors::Dict{String,Float64}
+  fit_curve::Function
+  model_str::String
 end
 
 """
@@ -43,14 +43,14 @@ Helper function to return a fit to a model.
 function generic_fit(xpts, ypts, model, initial_guess, fit_params, model_string::String; yvars=[])
 	@assert length(xpts) == length(ypts) "X and Y data length must match."
 	if isempty(yvars)
-			result = curve_fit(model, xpts, ypts, initial_guess)
-			errors = estimate_errors(result)
-			sq_error = sum(((model(xpts, result.param) - ypts).^2))
+		result = curve_fit(model, xpts, ypts, initial_guess)
+		errors = estimate_errors(result)
+		sq_error = sum(((model(xpts, result.param) - ypts).^2))
 	else
-			@assert length(ypts) == length(yvars) "Y data and Y variance lengths must match."
-			result = curve_fit(model, xpts, ypts, 1./sqrt(yvars), initial_guess)
-			errors = estimate_errors(result)
-			sq_error = sum(((model(xpts, result.param) - ypts).^2)./yvars)
+		@assert length(ypts) == length(yvars) "Y data and Y variance lengths must match."
+		result = curve_fit(model, xpts, ypts, 1./sqrt(yvars), initial_guess)
+		errors = estimate_errors(result)
+		sq_error = sum(((model(xpts, result.param) - ypts).^2)./yvars)
 	end
 	# Compute badness of fit:
 	# Under the null hypothesis (that the model is valid and that the observations
@@ -69,14 +69,14 @@ function generic_fit(xpts, ypts, model, initial_guess, fit_params, model_string:
 end
 
 """ `fit_t1(xpts, ypts, yvars=[])`
-Fit to exponential T₁ decay y = a exp(-t/T₁) + b.
+Fit to exponential T decay y = a exp(-t/T) + b.
 """
 function fit_t1(xpts, ypts, yvars=[])
-	T1_fit_dict(p) = Dict("a" => p[1], "T₁" => p[2], "b" => p[3])
+	T1_fit_dict(p) = Dict("a" => p[1], "T" => p[2], "b" => p[3])
 	model(t, p) = p[1]*exp.(-t ./ p[2]) + p[3]
 	p_guess = [ypts[1]-ypts[end], xpts[end]/3., ypts[end]]
 	return generic_fit(xpts, ypts, model, p_guess, T1_fit_dict,
-										"a * exp(-t/T₁) + b", yvars=yvars)
+										"a * exp(-t/T) + b", yvars=yvars)
 end
 
 """ `fit_line(xpts, ypts, yvars=[])`
@@ -99,14 +99,14 @@ with optional weights for each point.
 """
 function fit_ramsey(xpts, ypts, yvars=[])
 
-		fit_dict1(p) = Dict("a"=>p[1], "T"=>p[2], "f"=>p[3], "ϕ"=>p[4], "b"=>p[5])
-    model(t, p) = p[1]*exp.(-t ./ p[2]).*cos.(2π*p[3] .* t + p[4]) + p[5]
-    # Use KT estimation to get a guess for the fit
-    freqs,Ts,amps = KT_estimation(ypts-mean(ypts), xpts[2]-xpts[1], 1)
-    p_guess = [abs(amps[1]), Ts[1], freqs[1], angle(amps[1]), mean(ypts)];
+	fit_dict1(p) = Dict("a"=>p[1], "T"=>p[2], "f"=>p[3], "ϕ"=>p[4], "b"=>p[5])
+  model(t, p) = p[1]*exp.(-t ./ p[2]).*cos.(2π*p[3] .* t + p[4]) + p[5]
+  # Use KT estimation to get a guess for the fit
+  freqs,Ts,amps = KT_estimation(ypts-mean(ypts), xpts[2]-xpts[1], 1)
+  p_guess = [abs(amps[1]), Ts[1], freqs[1], angle(amps[1]), mean(ypts)];
 
-		return generic_fit(xpts, ypts, model, p_guess,
-											fit_dict1, "a*exp(-t/T).*cos(2πf .* t + ϕ) + b)", yvars=yvars)
+	return generic_fit(xpts, ypts, model, p_guess,
+										fit_dict1, "a*exp(-t/T).*cos(2πf .* t + ϕ) + b)", yvars=yvars)
 
 end
 
@@ -125,35 +125,35 @@ used based on Akaike's Information Criterion (AIC). This assumes
 Gaussian statistics for each observation.
 """
 function fit_twofreq_ramsey(xpts, ypts, yvars=[])
-		fit_dict2(p) = Dict("a₁"=>p[1], "T₁"=>p[2], "f₁"=>p[3], "ϕ₁"=>p[4],
-	                    "a₂"=>p[5], "T₂"=>p[6], "f₂"=>p[7], "ϕ₂"=>p[8],
-	                    "b"=>p[9])
-    model2(t, p) = ( p[1] * exp.(-t ./ p[2]) .* cos.(2π * p[3] .*t + p[4]) +
-                    p[5] * exp.(-t ./ p[6]) .* cos.(2π * p[7] .*t + p[8]) + p[9] )
+	fit_dict2(p) = Dict("a₁"=>p[1], "T₁"=>p[2], "f₁"=>p[3], "ϕ₁"=>p[4],
+                    "a₂"=>p[5], "T₂"=>p[6], "f₂"=>p[7], "ϕ₂"=>p[8],
+                    "b"=>p[9])
+  model2(t, p) = ( p[1] * exp.(-t ./ p[2]) .* cos.(2π * p[3] .*t + p[4]) +
+                  p[5] * exp.(-t ./ p[6]) .* cos.(2π * p[7] .*t + p[8]) + p[9] )
 
-    #Use KT estimation to get a guess for the fit
-    freqs,Ts,amps = KT_estimation(ypts-mean(ypts), xpts[2]-xpts[1], 2)
-    phases = angle(amps)
-    amps = abs(amps)
-    p_guess = [amps[1], Ts[1], freqs[1], phases[1], amps[2], Ts[2], freqs[2], phases[2], mean(ypts)]
+  #Use KT estimation to get a guess for the fit
+  freqs,Ts,amps = KT_estimation(ypts-mean(ypts), xpts[2]-xpts[1], 2)
+  phases = angle(amps)
+  amps = abs(amps)
+  p_guess = [amps[1], Ts[1], freqs[1], phases[1], amps[2], Ts[2], freqs[2], phases[2], mean(ypts)]
 
-    fitresult2 = generic_fit(xpts, ypts, model2, p_guess, fit_dict2,
-									"a₁*exp(-t ./ T₁).*cos(2πf₁ .* t + ϕ₁) + a₂*exp(-t ./ T₂).*cos(2πf₂ .* t + ϕ₂) + b",
-									yvars=yvars)
+  fitresult2 = generic_fit(xpts, ypts, model2, p_guess, fit_dict2,
+								"a₁*exp(-t ./ T₁).*cos(2πf₁ .* t + ϕ₁) + a₂*exp(-t ./ T₂).*cos(2πf₂ .* t + ϕ₂) + b",
+								yvars=yvars)
 
-    fitresult1 = fit_ramsey(xpts, ypts, yvars)
+  fitresult1 = fit_ramsey(xpts, ypts, yvars)
 
-    # AIC computation
-    k1 = 5
-    k2 = 9
-    corr(k,n) = (k+1)*(k+1)/(n-k-2)
-    aicc(e,k,n) = 2 * k + e + corr(k,n)
+  # AIC computation
+  k1 = 5
+  k2 = 9
+  corr(k,n) = (k+1)*(k+1)/(n-k-2)
+  aicc(e,k,n) = 2 * k + e + corr(k,n)
 
-    aic = aicc(fitresult1.sq_error,k2,length(xpts)) - aicc(fitresult1.sq_error,k1,length(xpts))
+  aic = aicc(fitresult1.sq_error,k2,length(xpts)) - aicc(fitresult1.sq_error,k1,length(xpts))
 
-    return [ fitresult1, fitresult2,
-           (aic > 0) ? 1 : 2,
-           aic ]
+  return [ fitresult1, fitresult2,
+         (aic > 0) ? 1 : 2,
+         aic ]
 end
 
 """`fit_sin(xpts, ypts, yvars)`
