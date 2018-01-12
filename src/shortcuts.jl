3dd570142235c5e30fbb4891fddeef4b5ba86bde
Diego Ristè <diego.riste@raytheon.com>
2018-01-12 13:36:11 -0500

    Fix cal_data indices

--- src/shortcuts.jl
+++ src/shortcuts.jl
@@ -10,7 +10,7 @@ num_repeats: number of calibration points per computational state
 """
 function cal_data(data::Array; bit = 1, nqubits = 1, num_repeats = 2)
 	zero_cal = mean(data[end-num_repeats*(2^nqubits)+1:end-num_repeats*(2^nqubits-1)])
-	one_cal = mean(data[end-num_repeats*(2^nqubits-2^(bit-1)):end-num_repeats*(2^nqubits-2^(bit-1)-1)-1])
+	one_cal = mean(data[end-num_repeats*(2^nqubits-2^(bit-1))+1:end-num_repeats*(2^nqubits-2^(bit-1)-1)])
 	scale_factor = -(one_cal - zero_cal)/2;
 	data = data[1:end-2*num_repeats]
 	data = (data - zero_cal)/scale_factor + 1
