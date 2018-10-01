987673a956fc92f6248492399e3b9bf618d305cb
Diego Ristè <diego.riste@raytheon.com>
2018-10-01 15:56:29 -0400

    Fit to CR hamiltonian (#41)
    * Fit to CR hamiltonian
    
    Following Sheldon et al., PRA 93 (2016)

--- src/common_fits.jl
+++ src/common_fits.jl
@@ -239,3 +239,24 @@ function fit_RB(xpts, ypts, yvars=[])
     p_guess = [0.5, .01, 0.5]
     return generic_fit(xpts, ypts, model, p_guess, RB_fit_dict, "a * exp(-n/b) + c", yvars=yvars)
 end
+
+function calculate_residueCR(params, t, rvec)
+    rx        = params[1]; # Rabi frequency along x
+    ry        = params[2]; # Rabi frequency along y
+    delta     = params[3]; # detuning
+    x0        = params[4]; # initial x
+    y0        = params[5]; # initial y
+    z0        = params[6]; # initial z
+    omega = sqrt(rx^2+ry^2+delta^2);
+    x = (rx*delta*(-z0+x0*rx+y0*ry)+(rx*(z0*delta-y0*ry)+x0*(delta^2+ry^2)).*cos.(t*omega)+omega*(y0*delta+z0*ry).*sin.(t*omega))/omega^2;
+    y = (ry*delta*(-z0+x0*rx+y0*ry)+(ry*(z0*delta-x0*rx)+y0*(delta^2+rx^2)).*cos.(t*omega)-omega*(x0*delta+z0*rx).*sin.(t*omega))/omega^2;
+    z = (delta*(z0*delta-x0*rx-y0*ry)+(x0*delta*rx+y0*delta*ry+z0*(rx^2+ry^2)).*cos.(t*omega)+omega*(y0*rx-x0*ry).*sin.(omega*t))/omega^2;
+    rmodel = [x,y,z];
+    diffvec = rvec-rmodel;
+    return sum(hcat(diffvec...).^2), rmodel
+end
+
+function fit_CR_hamiltonian(xpoints, datavec, params_estimate = [5,5,0,0,0,1])
+    result = optimize(x-> calculate_residueCR(x, xpoints, datavec)[1], params_estimate)
+    return Optim.minimizer(result)
+end
